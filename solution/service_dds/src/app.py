6358a207cb317996f6026ff12c257e50588b9ef3
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-14 23:46:12 +0500

    v0.44

--- solution/service_dds/src/app.py
+++ solution/service_dds/src/app.py
@@ -5,6 +5,9 @@ from flask import Flask
 
 from app_config import AppConfig
 from dds_loader.dds_message_processor_job import DdsMessageProcessor
+from dds_loader.outbox_publisher_job import OutboxPublisher
+from dds_loader.repository.dds_repository import DdsRepository
+
 
 app = Flask(__name__)
 
@@ -19,13 +22,40 @@ def hello_world():
 if __name__ == '__main__':
     app.logger.setLevel(logging.DEBUG)
 
+    repository = DdsRepository(
+        config.pg_warehouse_db()
+    )
+
     proc = DdsMessageProcessor(
         config.kafka_consumer(),
+        repository,
+        app.logger
+    )
+
+    outbox_publisher = OutboxPublisher(
+        config.kafka_producer(),
+        repository,
         app.logger
     )
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=proc.run, trigger="interval", seconds=25)
+
+    scheduler.add_job(
+        func=proc.run,
+        trigger="interval",
+        seconds=25
+    )
+
+    scheduler.add_job(
+        func=outbox_publisher.run,
+        trigger="interval",
+        seconds=5
+    )
+
     scheduler.start()
 
-    app.run(debug=True, host='0.0.0.0', use_reloader=False)
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        use_reloader=False
+    )
