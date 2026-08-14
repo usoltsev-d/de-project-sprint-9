f880f118b918228b80af3381a5324958a561ae8e
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-15 00:10:04 +0500

    v0.46

--- solution/service_stg/src/app_config.py
+++ solution/service_stg/src/app_config.py
@@ -15,11 +15,11 @@ class AppConfig:
         self.kafka_port = int(str(os.getenv('KAFKA_PORT')) or 0)
         self.kafka_consumer_username = str(os.getenv('KAFKA_CONSUMER_USERNAME') or "")
         self.kafka_consumer_password = str(os.getenv('KAFKA_CONSUMER_PASSWORD') or "")
-        self.kafka_consumer_group = str(os.getenv('KAFKA_CONSUMER_GROUP') or "")
+        self.kafka_consumer_group = str(os.getenv('KAFKA_STG_CONSUMER_GROUP') or "")
         self.kafka_consumer_topic = str(os.getenv('KAFKA_SOURCE_TOPIC') or "")
         self.kafka_producer_username = str(os.getenv('KAFKA_CONSUMER_USERNAME') or "")
         self.kafka_producer_password = str(os.getenv('KAFKA_CONSUMER_PASSWORD') or "")
-        self.kafka_producer_topic = str(os.getenv('KAFKA_DESTINATION_TOPIC') or "")
+        self.kafka_producer_topic = str(os.getenv('KAFKA_STG_SERVICE_ORDERS_TOPIC') or "")
 
         self.redis_host = str(os.getenv('REDIS_HOST') or "")
         self.redis_port = int(str(os.getenv('REDIS_PORT')) or 0)
