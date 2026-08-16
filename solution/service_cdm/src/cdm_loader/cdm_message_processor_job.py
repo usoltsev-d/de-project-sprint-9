881ad1a5dc69fd27c567c27cecf952e64d3faee8
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-16 22:15:12 +0500

    v0.61

--- solution/service_cdm/src/cdm_loader/cdm_message_processor_job.py
+++ solution/service_cdm/src/cdm_loader/cdm_message_processor_job.py
@@ -1,19 +1,42 @@
 from datetime import datetime
 from logging import Logger
-from uuid import UUID
 
 from lib.kafka_connect import KafkaConsumer
+from cdm_loader.repository.cdm_repository import CdmRepository
 
 
 class CdmMessageProcessor:
-    def __init__(self,
-                 logger: Logger,
-                 ) -> None:
-
+    def __init__(
+        self,
+        consumer: KafkaConsumer,
+        repository: CdmRepository,
+        logger: Logger
+    ) -> None:
+        self._consumer = consumer
+        self._repository = repository
         self._logger = logger
         self._batch_size = 100
 
     def run(self) -> None:
         self._logger.info(f"{datetime.utcnow()}: START")
+
+        for _ in range(self._batch_size):
+            message = self._consumer.consume()
+
+            if message is None:
+                break
+
+            try:
+                # Обновляем витрины CDM
+                self._repository.save_order(message)
+
+                # Фиксируем offset только после успешной записи в БД
+                self._consumer.commit()
+
+            except Exception:
+                self._logger.exception(
+                    "Ошибка обработки сообщения из Kafka"
+                )
+                break
 
         self._logger.info(f"{datetime.utcnow()}: FINISH")
